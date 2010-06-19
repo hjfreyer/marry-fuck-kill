43a5f91e76f60709886e16ab5b08cdad33cb1d2d
Michael Kelly <michael@michaelkelly.org>
2010-06-19 19:45:02 -0400

    Can select an image result in UI.
    Layout exists for displaying triple, not functional yet.

--- MfkMaker/src/com/mfk/web/maker/client/MfkMaker.java
+++ MfkMaker/src/com/mfk/web/maker/client/MfkMaker.java
@@ -1,5 +1,7 @@
 package com.mfk.web.maker.client;
 
+import java.util.Vector;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JsArray;
@@ -27,6 +29,7 @@ import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.UIObject;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
@@ -36,6 +39,10 @@ public class MfkMaker implements EntryPoint {
 	/**
 	 * This is the entry point method.
 	 */
+	
+	public static Vector<Image> results = new Vector<Image>();
+	public static Image selected;
+	
 	public void onModuleLoad() {
 		final RootPanel resultPanel = RootPanel.get("search-results");
 		
@@ -52,6 +59,7 @@ public class MfkMaker implements EntryPoint {
 			@Override
 			public void onSearchStarting(SearchStartingEvent event) {
 				resultPanel.clear();
+				MfkMaker.results.clear();
 			}
 	    });
 	    control.addSearchResultsHandler(new SearchResultsHandler() {
@@ -67,18 +75,40 @@ public class MfkMaker implements EntryPoint {
 					thumb.addStyleName("search-result");
 					thumb.addClickHandler(resultClick);
 					resultPanel.add(thumb);
+					MfkMaker.results.add(thumb);
 				}
 			}
 	    });
 	    control.execute("treehouse");
 	    RootPanel.get("search-control").add(control);
+	    
+	    final Button set1 = new Button("Set Item 1");
+	    final Button set2 = new Button("Set Item 2");
+	    final Button set3 = new Button("Set Item 3");
+	    
+	    set1.setEnabled(false);
+	    set2.setEnabled(false);
+	    set3.setEnabled(false);
+	    
+	    RootPanel.get("saved-1").add(set1);
+	    RootPanel.get("saved-2").add(set2);
+	    RootPanel.get("saved-3").add(set3);
 	}
 }
 
 class ResultClickHandler implements ClickHandler {
 	@Override
 	public void onClick(ClickEvent event) {
+		for (Image img: MfkMaker.results) {
+			img.setStylePrimaryName("search-result");
+		}
+		
 		Image source = (Image)event.getSource();
+		source.setStylePrimaryName("search-result-sel");
+		MfkMaker.selected = source;
+
 		System.out.println("Clicked on " + source.getUrl());
 	}
 }
+
+
