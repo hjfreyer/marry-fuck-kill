4dde0bdbf120dd7e97c50f3f20339c1af825b875
Michael Kelly <michael@michaelkelly.org>
2012-02-11 22:24:46 -0500

    Add commented-out stats config and URL handler.

---
+++ marry-fuck-kill/appengine_config.py
@@ -0,0 +1,5 @@
+# Uncomment this to enable stats (also uncomment URL handler in app.yaml):
+#def webapp_add_wsgi_middleware(app):
+#   from google.appengine.ext.appstats import recording
+#   app = recording.appstats_wsgi_middleware(app)
+#   return app
