5385944ebf38b7d0a60c7573b70e75422fe6807b
Hunter Freyer <yt@hjfreyer.com>
2012-11-01 18:29:42 -0400

    Restructures the code into something more logical, and adds a script to put it into the way appengine wants it. There'll be a corresponding one for making a production-ready version at some point in the future

--- v2/dev_deploy.sh
+++ v2/dev_deploy.sh
@@ -2,14 +2,27 @@
 
 trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
 
+DEV_APPSERVER="/Users/hjfreyer/tools/google_appengine/dev_appserver.py"
+
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BIN="$DIR/bin/dev"
+TMP=$(mktemp -d -t mfk)
 
-mkdir -p $DIR/bin/dev
-mkdir -p $DIR/bin/static
+rm -rf $BIN
 
+mkdir -p $BIN
+mkdir -p $BIN/static
 
+ln -s $DIR/app.yaml $BIN
 
-xeyes&
-xeyes&
+ln -s $DIR/go $BIN/gomfk
+ln -s $DIR/third_party/closure-library $BIN/static/closure-library
 
-pause
+ln -s $DIR/js/script.js $BIN/static/
+
+sass \
+    --watch $DIR/stylesheets/:$BIN/static/ \
+    --cache-location \
+    $TMP/sass > $TMP/sass.log &
+
+$DEV_APPSERVER $BIN
