948308eed07c103fc04b5ffc1ddc6332a0807bdc
Hunter Freyer <yt@hjfreyer.com>
2012-11-02 15:55:31 -0400

    Removes v1 from the v2 branch, moves code from the v2 directory to the top level.

--- MfkMaker/src/com/mfk/web/maker/client/event/ImageResultsHandler.java
+++
@@ -1,9 +0,0 @@
-package com.mfk.web.maker.client.event;
-
-import java.util.List;
-
-import com.mfk.web.maker.client.model.ImageInfo;
-
-public interface ImageResultsHandler {
-  public void handleImageResults(String query, List<ImageInfo> results);
-}
