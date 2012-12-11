382745126dab4a4db92209715c025e0fbe05f64f
Hunter Freyer <yt@hjfreyer.com>
2012-12-10 22:06:44 -0500

    checkpoint

---
+++ go/mfklib/deps.go
@@ -0,0 +1,60 @@
+package mfklib
+
+import (
+	"fmt"
+)
+
+type TripleId int64
+type UserId string
+
+type EntityNotFoundError struct {
+	Type string
+	Id   int64
+	Err  error
+}
+
+func (e EntityNotFoundError) Error() string {
+	return fmt.Sprintf("Entity of type %q with id %d not found. Error: %s",
+		e.Type, e.Id, e.Err)
+}
+
+type IllegalArgumentError struct {
+	Func     string
+	Argument string
+	Value    interface{}
+	Cause    string
+}
+
+func (e IllegalArgumentError) Error() string {
+	return fmt.Sprintf("Function %q got illegal value %q for argument %q: %s",
+		e.Func, e.Value, e.Argument, e.Cause)
+}
+
+type Logger interface {
+	Infof(format string, args ...interface{})
+	Warningf(format string, args ...interface{})
+}
+
+type ImageSearcher interface {
+	Search(query string) ([]*ImageMetadata, error)
+}
+
+type ImageOrError struct {
+	*Image
+	error
+}
+
+type ImageFetcher interface {
+	FetchImage(metadata *ImageMetadata) chan ImageOrError
+}
+
+type TripleStatsUpdater func(*TripleStats, *TripleUserStatus)
+
+type Database interface {
+	AddTriple(*Triple) (TripleId, error)
+	GetTriple(tripleId TripleId) (*Triple, error)
+
+	UpdateStats(tripleId TripleId, userId UserId, updater TripleStatsUpdater) (
+		*TripleStats, *TripleUserStatus, error)
+}
+
