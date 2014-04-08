a99443553260d3a0b953aa73169b18c759d1d8c0
hjfreyer <yt@hjfreyer.com>
2014-04-08 00:37:39 -0400

    Functioning MFK maker! Does searching and all that, but doesn't submit the triple to the server yet, that's next.
    Also, updates to the "build" script.

--- run-dev.sh
+++ run-dev.sh
@@ -5,11 +5,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 BIN=$DIR/bin-dev
 
-rm -rf $BIN
-mkdir $BIN
-mkdir $BIN/marry-fuck-kill
-ln -s $DIR/backend/* $BIN/marry-fuck-kill
-ln -s $DIR/frontend/app $BIN/marry-fuck-kill/jsapp
 
-sass --watch frontend/app/css/app.scss &
-dev_appserver.py $BIN/marry-fuck-kill
+rm -rf $BIN
+mkdir -p $BIN/
+
+ln -s $DIR/backend/* $BIN/
+
+mkdir -p $BIN/static2/app/
+ln -s $DIR/frontend/app/* $BIN/static2/app/
+rm $BIN/static2/app/css
+mkdir -p $BIN/static2/app/css
+sass --watch $DIR/frontend/app/css/app.scss:$BIN/static2/app/css/app.css &
+
+cd $DIR
+bower update
+cd -
+ln -s $DIR/bower_components $BIN/static2/bower
+
+dev_appserver.py $BIN
