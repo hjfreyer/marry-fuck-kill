3471fe72b539e0dc6739ab4a2dba5e8eccd2a94a
hjfreyer <yt@hjfreyer.com>
2014-04-14 14:55:42 -0400

    Remove confusing LICENSE and README duplication. Also delete some stray files, and tweak build script so it doesn't interfere with pushd/popd.

--- run-dev.sh
+++ run-dev.sh
@@ -4,7 +4,7 @@ trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 BIN=$DIR/bin-dev
-
+CD='command cd'
 
 rm -rf $BIN
 mkdir -p $BIN/
@@ -17,9 +17,9 @@ rm $BIN/static2/app/css
 mkdir -p $BIN/static2/app/css
 sass --watch $DIR/frontend/app/css/app.scss:$BIN/static2/app/css/app.css &
 
-cd $DIR
+$CD $DIR
 bower update
-cd -
+$CD -
 ln -s $DIR/bower_components $BIN/static2/bower
 
 dev_appserver.py $BIN
