4d25ad4cff9ebde13b3462b0b9fc54db85588c7f
hjfreyer <yt@hjfreyer.com>
2014-04-14 23:27:06 -0400

    commit

--- frontend/app/js/controllers.js
+++ frontend/app/js/controllers.js
@@ -2,17 +2,6 @@
 
 /* Controllers */
 
-var Page = ['$scope', 'StateStack', function($scope, StateStack) {
-    $scope.page = {
-	onKeyDown: function(event) {
-	    if (event.keyCode == 27) {  // esc
-		StateStack.pop();
-	    }
-	},
-	popState: StateStack.pop.bind(StateStack)
-    };
-}];
-
 var Entity = function(Searcher, StateStack) {
     this.searcher = Searcher;
     this.stateStack = StateStack;
@@ -28,17 +17,17 @@ var Entity = function(Searcher, StateStack) {
 
 Entity.prototype.showSearch = function() {
     this.stateStack.push(
-	function(imgIdx, query, images, imagesPromise) {
-	    this.imgIdx = imgIdx;
-	    this.query = query;
-	    this.images = images;
-	    this.imagesPromise = imagesPromise;
+        function(imgIdx, query, images, imagesPromise) {
+            this.imgIdx = imgIdx;
+            this.query = query;
+            this.images = images;
+            this.imagesPromise = imagesPromise;
 
-	    this.searching = false;
-	}.bind(this, this.imgIdx, this.query, this.images, this.imagesPromise),
-	function(activeElement) {
-	    activeElement.focus();
-	}.bind(this, document.activeElement));
+            this.searching = false;
+        }.bind(this, this.imgIdx, this.query, this.images, this.imagesPromise),
+        function(activeElement) {
+            activeElement.focus();
+        }.bind(this, document.activeElement));
 
     this.searching = true;
     this.query = this.name;
@@ -53,75 +42,98 @@ Entity.prototype.selectImage = function(idx) {
 
 Entity.prototype.getImage = function() {
     if (this.imgIdx == -1) {
-	return '/s/mfk.png';
+        return '/s/mfk.png';
     }
     return this.images[this.imgIdx].thumbnail;
 };
 
+Entity.prototype.isPlaceholder = function() {
+    return this.imgIdx == -1;
+};
+
 Entity.prototype.search = function() {
-    console.log('foo');
+    if (this.query == '') {
+        return;
+    }
+
     var imagesPromise = this.searcher(this.query);
     this.imagesPromise = imagesPromise;
     imagesPromise.then(function(result) {
-	if (this.imagesPromise != imagesPromise) {
-	    console.log('replaced');
-	    return;
-	}
-	console.log(result);
-	this.images = result.data.images;
+        if (this.imagesPromise != imagesPromise) {
+            return;
+        }
+        this.images = result.data.images;
     }.bind(this));
 };
 
-Entity.prototype.getReset = function() {
-    return function(name, imgIdx, images, position) {
-	this.name = name;
-	this.imgIdx = imgIdx;
-	this.images = images;
-	this.position = position;
-    }.bind(this, this.name, this.imgIdx, this.images, this.position);
-};
-
-Entity.prototype.imageUrls = function() {
-    this.images.map(function() {
-
-    }, this);
-};
-
-var MakerTable = ['$scope', 'Searcher', 'StateStack', function(
-    $scope, Searcher, StateStack) {
-    var entities = [];
+var Table = function(Searcher, StateStack) {
+    this.entities = [];
     for (var i = 0; i < 3; i++) {
-	entities[i] = new Entity(Searcher, StateStack);
+        this.entities[i] = new Entity(Searcher, StateStack);
     }
 
-    $scope.table = {
-	entities: entities,
+    this.errorCode = '';
+};
 
-	select: function(idx) {
-	    console.log(idx);
-	    console.log($scope.table.entities[idx]);
-	    var reset = $scope.table.entities[idx].getReset();
-	    $scope.table.entities[idx].searching = true;
-	    StateStack.push(function() {
-		$scope.table.entities[idx].searching = false;
-		reset();
-	    });
+Table.prototype.submit = function(event) {
+    if (this.entities[0].name == '' ||
+	this.entities[1].name == '' ||
+	this.entities[2].name == '') {
+	this.errorCode = 'NO_NAME';
+	console.log(event);
+	event.stopPropagation();
+	return;
+    }
+    if (this.entities[0].imgIdx == -1 ||
+	this.entities[1].imgIdx == -1 ||
+	this.entities[2].imgIdx == -1) {
+	this.errorCode = 'NO_IMAGE';
+	return;
+    }
+};
+
+var Page = ['$scope', 'Searcher', 'StateStack', function($scope, Searcher, StateStack) {
+    $scope.page = {
+        onKeyDown: function(event) {
+            if (event.keyCode == 27) {  // esc
+                StateStack.pop();
+            }
+        },
+        popState: StateStack.pop.bind(StateStack),
+	reset: function() {
+	    $scope.table = new Table(Searcher, StateStack);
 	},
-
-	showSearch: function(entity) {
-	    entity.position = 1;
-	},
-
-	search: function(entity) {
-	},
-
-	done: function() {
-	    $scope.table.searchIdx = -1;
-	    StateStack.release();
-	}
     };
+    $scope.table = new Table(Searcher, StateStack);
 }];
 
+
+// var MakerTable = ['$scope', '$timeout', 'Searcher', 'StateStack', function(
+//     $scope, $timeout, Searcher, StateStack) {
+//     var entities = [];
+
+//     $scope.table = {
+//         entities: entities,
+// 	errorCode: '',
+
+// 	reset: function() {
+// 	    $scope.table.entities = [];
+// 	    for (var i = 0; i < 3; i++) {
+// 		$scope.table.entities[i] = new Entity(Searcher, StateStack);
+// 	    }
+// 	},
+
+// 	submit: function() {
+// 	    if (entities[0].name == '' || entities[1].name == '' || entities[2].name == '') {
+// 		$scope
+// 	    }
+
+// 	    $scope.table.nameNeeded = true;
+// 	    $timeout(function() { $scope.table.nameNeeded = false; }, 5000);
+// 	},
+//     };
+// }];
+
 angular.module('mfkMaker.controllers', [])
-    .controller('Page', Page)
-    .controller('MakerTable', MakerTable);
+    .controller('Page', Page);
+//    .controller('MakerTable', MakerTable);
