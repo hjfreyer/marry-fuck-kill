957ff85acbae551e091648982b16656fc817f29c
Michael Kelly <michael@michaelkelly.org>
2016-11-27 19:19:24 -0500

    Gruntfile isn't working for bootstrap.css.
    I don't care to debug this right now, so I added shell:copy_css to work
    around it.
    
    We are almost certainly missing angular.js from the build/ output
    directory at this point. We could replace it with another 'shell'
    section if we have to. But we only use it in /make anyway, IIRC.

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,17 +39,11 @@ module.exports = function(grunt) {
     },
 
     bower: {
-      main: {
-        dest: 'build/static/deps/',
-        options: {
-          packageSpecific: {
-            bootstrap: {
-              dest: 'build/static/deps/fonts',
-              css_dest: 'build/static/deps/bootstrap'
-            }
-          }
-        }
-      }
+      main: {}
+      // XXX(mjkelly): This is totally bustd. Things end up in
+      // build/static/deps/bootstrap/bootstrap.css/*, etc. I don't care enough
+      // to fuss with it. See shell:copy_css below. See previous copies of this
+      // file for the original content of this section.
     },
 
     shell: {
@@ -59,6 +53,9 @@ module.exports = function(grunt) {
           async: true,
         },
       },
+      copy_css: {
+        command: 'mkdir -p build/static/deps/bootstrap && cp bower_components/bootstrap/dist/css/bootstrap.min.css build/static/deps/bootstrap/bootstrap.css'
+      }
     },
 
     watch: {
@@ -85,7 +82,7 @@ module.exports = function(grunt) {
   // define the tasks
   grunt.registerTask(
     'css', '',
-    [ 'sass', 'autoprefixer' ]
+    [ 'sass', 'autoprefixer', 'shell:copy_css' ]
   );
   grunt.registerTask(
     'build',
